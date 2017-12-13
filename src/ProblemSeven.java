bb00b1fe1afcdfaab085d5759f65f6a6eed92b3c
Jon Fincher <jfincher42@gmail.com>
2017-12-12 18:32:29 -0800

    Pushing partial solution to problem seven

--- src/ProblemSeven.java
+++ src/ProblemSeven.java
@@ -6,25 +6,41 @@ import java.util.Arrays;
 public class ProblemSeven {
 
 	public static void main(String[] args) {
+		// Open the input file
 		Scanner inFile;
 		try {
-			inFile = new Scanner(new File("input\\problem-seven.txt"));
+			inFile = new Scanner(new File("input/problem-seven.txt"));
 		}
 		catch (Exception e) {
 			return;
 		}
 		
+		// Read and parse each line, add each to an ArrayList
 		ArrayList<Tower> towers=new ArrayList<Tower>();
 		while (inFile.hasNextLine()) {
 			towers.add(parse(inFile.nextLine()));
 		}
 		inFile.close();
+		
+		// Now, process the list into a tree
+		// Visit each node with children
+		//  For each child, find the 
 	}
 	
 	public static Tower parse(String line) {
 		String[] items = line.split("[(),\\s]");
 		
-		Tower tower = new Tower();
+		System.out.println(line + " --> " + Arrays.toString(items));
+		
+		Tower tower = new Tower(items[0], Integer.parseInt(items[2]));
+		System.out.println("  Name: " + items[0] + ", weight: " + items[2]);
+		
+		if (items.length>3) {
+			for (int i=5; i<items.length; i+=2) {
+				System.out.println("  Adding child named: " + items[i]);
+				tower.addChild(new Tower(items[i]));
+			}
+		}
 		return tower;
 	}
 
