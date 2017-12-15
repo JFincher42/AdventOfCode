8d7f14b53bd8d5dc578d14c3143008f7aaed6e15
Jon Fincher <jfincher42@gmail.com>
2017-12-14 20:05:38 -0800

    Problem 10 Part two done - had to undo some of part one

--- src/PackageTen/ProblemTen.java
+++ src/PackageTen/ProblemTen.java
@@ -1,39 +1,71 @@
 package PackageTen;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class ProblemTen {
 
 	public static final int SIZE = 256; 
 	public static int[] list = new int[SIZE];
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 	
 		// First, initialize the list
 		for (int i=0; i<SIZE; i++) list[i]=i;
 		
-		Scanner inFile;
+		// Need to read character by character
+		BufferedReader inFile;
 		try {
-			inFile = new Scanner(new File("input/problem-ten.txt"));
-			inFile.useDelimiter(",");
+			inFile = new BufferedReader(new FileReader("input/problem-ten.txt"));
 		}
 		catch (Exception e) {
 			return;
 		}
 		
+		// Build an ArrayList out of the input string
+		ArrayList<Integer> inputString = new ArrayList<Integer> ();
+		int inputChar;
+		while (-1 != (inputChar=inFile.read())) {
+			inputString.add(inputChar);
+		}
+		// Add the final five items
+		inputString.add(17);
+		inputString.add(31);
+		inputString.add(73);
+		inputString.add(47);
+		inputString.add(23);
+		
+		inFile.close();
+		
+		// Start everything at zero
 		int current=0, skip=0;
-		while (inFile.hasNextInt()) {
-			int length = inFile.nextInt();
-//			System.out.println("Length = " + length);
-			twist(current, length, skip);
-			current += length+skip++;
-			if (current >= SIZE) current-=SIZE;
-//			System.out.println(Arrays.toString(list));
+		// But now we do 64 rounds
+		int round = 64;
+		while (round>0) {
+			// Loop through all the items in the inputString
+			for (int i=0; i<inputString.size(); i++) {
+				int length = inputString.get(i);
+				twist(current, length, skip);
+				current += length+skip++;
+				while (current >= SIZE) current-=SIZE;
+			}
+			round--;
 		}
 		
-		System.out.println(list[0]*list[1]);
-		inFile.close();
+		// Now we convert this to a dense hash
+		int[] dense = new int[16];
+		for (int block = 0; block<16; block++) {
+			int start = block*16;
+			int end = start + 16;
+			dense[block] = list[start];
+			for (int item=start+1; item<end; item++) dense[block] = dense[block] ^ list[item];
+			System.out.format("%x", dense[block]);
+		}
+		
+		
 	}
 	
 	public static void twist(int start, int length, int skip) {
