3dcb0216e034d9100ef62fd0afa26d66358b32ed
Jon Fincher <jfincher42@gmail.com>
2018-01-02 16:18:18 -0800

    Updated code to solve part 2

--- src/PackageSeventeen/ProgramSeventeen.java
+++ src/PackageSeventeen/ProgramSeventeen.java
@@ -3,38 +3,49 @@ package PackageSeventeen;
 import java.util.ArrayList;
 
 public class ProgramSeventeen {
-	public static final int STEP = 3;
-	public static ArrayList<Integer> buffer = new ArrayList<Integer>();
+	public static final int STEP = 354;
+	public static ArrayList<Integer> buffer = new ArrayList<Integer>(50000000);
+	//public static int[] buffer = new int[50000000];
 
 	public static void main(String[] args) {
 		int insertionPoint = 0;
+		//buffer[insertionPoint] = 0;
 		buffer.add(0);
-		for (int count = 1; count<=2017; count++) {
+
+		for (int count = 1; count<=50000000; count++) {
 			insertionPoint = insert(count, insertionPoint);
+			if (count%100000 == 0) System.out.print(".");
 		}
-		System.out.println(buffer.get(insertionPoint+1));
+		
+		System.out.println(buffer.get(buffer.indexOf(0)+1));
+		//System.out.println("\n" + buffer[0] + ", " + buffer[1]);
+		
 	}
 
 	private static int insert(int value, int point) {
+		//int length = value;
+		//int step = STEP;
 		int length = buffer.size();
-		int step = STEP;
 		
 		// If we have < STEP items in the list, we're going to wrap around
 		// So let's figure out the wrap
-		if (length <= STEP) {
-			step = STEP % length;
-		}
+//		if (length < STEP) {
+//			step = STEP % length;
+//		}
 		
 		// Now we just move to the insertion point
-		point += step;
-		if (point >= length)
-			point %= length;
+		point = (point + STEP) % length;
 		
-		if (point == length-1)
-			buffer.add(value);
-		else
-			buffer.add(point+1, value);
+//		for (int i=length; i>point; i--)
+//			buffer[i] = buffer[i-1];
 		
+//		buffer[point+1]=value;
+//		if (point == length-1)
+//			buffer.add(value);
+//		else
+//			buffer.add(point+1, value);
+//		
+		buffer.add(point, value);
 		return point+1;
 	}
 	
