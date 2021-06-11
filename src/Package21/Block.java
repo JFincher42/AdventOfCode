499ef6d70527275daf7ad66efa08cac78f440145
Jon Fincher <jfincher42@gmail.com>
2021-06-10 20:28:38 -0500

    Setup in IntelliJ

--- src/Package21/Block.java
+++ src/Package21/Block.java
@@ -8,31 +8,37 @@ public class Block {
 	
 	public Block(int h, int w) {
 		this.height = h;  this.width = w;
-		block = new boolean[height][width];
+		this.block = new boolean[height][width];
 	}
 	
 	// Appends another array of the same height to this one, returning the result.
 	public boolean[][] append(boolean[][] next) {
-		boolean [][] newArray = new boolean[height][width*2];
-		for (int i=0; i<height; i++) {
-			for (int j=0; j<width; j++) {
-				newArray[i][j]	     = this.block[i][j];
-				newArray[i][j+width] = next[i][j];
+		boolean [][] newArray = new boolean[this.height][this.width*2];
+		for (int i=0; i<this.height; i++) {
+			for (int j=0; j<this.width; j++) {
+				newArray[i][j]	          = this.block[i][j];
+				newArray[i][j+this.width] = next[i][j];
 			}
 		}
 		return newArray;
 	}
 
-	// Stacks this array on top of another one of the samewidth, returning the result
+	// Stacks this array on top of another one of the same width, returning the result
 	public boolean[][] stack(boolean[][] bottom)
 	{
-		boolean[][] newArray = new boolean[height*2][width];
-		for (int i=0; i<height; i++) {
-			for (int j=0; j<width; j++) {
-				newArray[i][j] 	      = this.block[i][j];
-				newArray[i+height][j] = bottom[i][j];
+		boolean[][] newArray = new boolean[this.height*2][this.width];
+		for (int i=0; i<this.height; i++) {
+			for (int j=0; j<this.width; j++) {
+				newArray[i][j] 	           = this.block[i][j];
+				newArray[i+this.height][j] = bottom[i][j];
 			}
 		}
 		return newArray;
+	}
+
+	// Compares this block against the pattern
+	public boolean compare(boolean[][] pattern)
+	{
+
 	}
 }
